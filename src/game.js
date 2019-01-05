4411a2b1ba9dad50b5910505b7a12ecba9deb007
Matt Murphy <matt@mattmurphy.ca>
2019-01-04 23:22:15 -0500

    add movement, basic shooting functionality

--- src/game.js
+++ src/game.js
@@ -18,41 +18,92 @@ function setupCanvas(canvas) {
 class Player {
 	constructor() {
 		this.xPos = 1 * px
-		this.yPos = 10 * px
+		this.yPos = 60 * px
+		this.width = 20
+		this.height = 12
+		this.topWidth = this.width / 4
+		this.topHeight = this.height / 1.8
+		this.topXPos = this.xPos + this.width / 2 - this.topWidth / 2
+		this.bulletYPos = this.yPos
+		this.speed = 4
+		this.moving = false
+		this.color = '#e5e5e5'
+	}
+	setTopXPos() {
+		this.topXPos = this.xPos + this.width / 2 - this.topWidth / 2
 	}
 	moveLeft() {
-		this.xPos -= 10
+		let futurePos = this.xPos - this.speed
+		if (futurePos > 0) this.xPos = futurePos
+		this.setTopXPos()
 	}
 	moveRight() {
-		this.xPos += 10
+		let futurePos = this.xPos + this.speed
+		if (futurePos < canvas.width - this.width) this.xPos = futurePos
+		this.setTopXPos()
+	}
+	moveIfKeyHeld() {
+		if (this.moving !== false) {
+			this.moving === 'right' ? this.moveRight() : this.moveLeft()
+		}
 	}
 	draw() {
-		ctx.fillStyle = '#e5e5e5'
-		let width = 20
-		let height = 12
-		let topWidth = width / 4
-		let topHeight = height / 1.8
-		ctx.fillRect(this.xPos, this.yPos + topHeight, width, height)
-		ctx.fillRect(this.xPos + width / 2 - topWidth / 2, this.yPos, topWidth, topHeight)
+		this.drawPlayer()
+		this.drawBullet()
 	}
-}
-
-function handleKeyDown(e) {
-	console.log(e.key)
-	if (e.key === a || e.key === ArrowLeft) player.moveLeft()
-	if (e.key === d || e.key === ArrowRight) player.moveRight()
+	drawBullet() {
+		ctx.fillStyle = 'red'
+		let width = this.width / 4
+		let height = this.height / 1.8
+		ctx.fillRect(this.bulletXPos, this.bulletYPos, width, height)
+	}
+	drawPlayer() {
+		ctx.fillStyle = this.color
+		// drawBottom
+		ctx.fillRect(this.xPos, this.yPos + this.topHeight, this.width, this.height)
+		// drawTop
+		ctx.fillRect(this.topXPos, this.yPos, this.topWidth, this.topHeight)
+	}
+	shoot() {
+		console.log('POW!')
+	}
+	startShooting() {
+		if (!this.shootingInterval) {
+			this.bulletYPos = this.yPos - this.topHeight
+			this.bulletXPos = this.topXPos
+			this.shoot()
+			this.shootingInterval = window.setTimeout(() => {
+				window.clearTimeout(this.shootingInterval)
+				this.shootingInterval = undefined
+			}, 500)
+		}
+	}
+	handleKeyDown(e) {
+		if (e.key === 'a' || e.key === 'ArrowLeft') this.moving = 'left'
+		if (e.key === 'd' || e.key === 'ArrowRight') this.moving = 'right'
+		if (e.key === ' ') this.startShooting()
+	}
+	handleKeyUp(e) {
+		// only stop moving in direction if keyup matches current direction
+		const keyMovesLeft = e.key === 'a' || e.key === 'ArrowLeft'
+		const keyMovesRight = e.key === 'd' || e.key === 'ArrowRight'
+		if (keyMovesLeft && this.moving === 'left') this.moving = false
+		if (keyMovesRight && this.moving === 'right') this.moving = false
+	}
 }
 
 function tick() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
-	// simulate movement
-	player.move()
+	player.moveIfKeyHeld()
+	player.bulletYPos -= 1
+	player.draw()
+	requestAnimationFrame(tick)
 }
 
 function startClock() {
 	const request = requestAnimationFrame(tick)
-	player.draw(player.xPos, 10.5)
-	window.addEventListener('keydown', handleKeyDown)
+	window.addEventListener('keydown', (e) => player.handleKeyDown(e))
+	window.addEventListener('keyup', (e) => player.handleKeyUp(e))
 }
 
 function setupGame() {
